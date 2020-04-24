3ecf7493f934bf9449dbddb0df6fa337ba804177
Benji <benjitobias@gmail.com>
2020-04-24 18:27:03 +0300

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+app.jinja_env.trim_blocks = True
+app.jinja_env.ltrim_blocks = True
+app.jinja_env.lstrip_blocks = True
+app.jinja_env.strip_blocks = True
+
+bootstrap = Bootstrap(app)
 
 from app import routes
